e9f3bb5263858585653a468409a70a42db191a67
Crowdbotics <team@crowdbotics.com>
2024-11-29 04:13:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "transfert-money-50173",
+  url: "https://transfert-money-50173.botics.co",
+  api: "https://transfert-money-50173.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
